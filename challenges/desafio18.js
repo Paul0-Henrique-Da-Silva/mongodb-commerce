6ebb8cd9f67d02a5f161fcb117bf98ef9db39689
Paulo Henrique <ph46163835@gmail.com>
2022-11-10 14:45:57 -0300

    desafio 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,5 @@
+db.produtos.update(
+    { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+    { $addToSet: { ingredientes: "bacon" } },
+);
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
