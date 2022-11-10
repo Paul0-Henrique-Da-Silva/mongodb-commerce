22645eba4baf941c834bd6dc91b56c6e0225b9ef
Paulo Henrique <ph46163835@gmail.com>
2022-11-10 12:57:33 -0300

    desafio 12 refactor

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,6 +1,6 @@
 db.produtos
   .updateMany(
-    { $nor: [{ name: "McChicken" }, { ingredientes: "ketchup" }] }, 
+    { $nor: [{ ingredientes: "ketchup" }, { name: "McChicken" }] }, 
     { $addToSet: { ingredientes: "ketchup" } },
     );
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
