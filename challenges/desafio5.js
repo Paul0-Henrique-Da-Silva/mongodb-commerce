98d2cc55ba7a618a570759bccadf3fd62295da40
Paulo Henrique <ph46163835@gmail.com>
2022-11-09 21:09:38 -0300

    fix: deasafio 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -3,5 +3,5 @@ db.produtos
     {
      $or: [{ curtidas: 36 }, { vendidos: 85 }],
     }, 
-    { _id: 0, nome: 1, curtidas: 1 },
+    { _id: 0, nome: 1, curtidas: 1, vendidos: 1 },
     );
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -3,5 +3,5 @@ db.produtos
     {
       curtidas: { $gt: 10, $lt: 100 },
     }, 
-    { _id: 0, nome: 1, vendidos: 1 },
+    { _id: 0, nome: 1, curtidas: 1 },
     );
