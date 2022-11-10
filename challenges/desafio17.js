6aab563021c6c36e402d7f0bf82c58f6fa3034ab
Paulo Henrique <ph46163835@gmail.com>
2022-11-10 14:27:49 -0300

    fix desafio 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,6 +1,6 @@
 db.resumoProdutos.insertOne({
     franquia: "McDonalds",
-    totalProdutos: db.produtos.countDocument({}),
+    totalProdutos: db.produtos.countDocuments({}),
 });
 
 db.resumoProdutos.find({}, { _id: 0 });
