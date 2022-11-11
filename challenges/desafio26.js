c0c7b6617b1d95d37e33caccb189e2e92504af7e
Paulo Henrique <ph46163835@gmail.com>
2022-11-11 00:35:06 -0300

    fix desafio 26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -2,6 +2,6 @@ db.produtos.updateMany(
     { valoresNutricionais: { 
         $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } }, 
     } },
-    { $push: { tags: { $each: ["muito sódio"] } } },
+    { $push: { tags: { $each: ["contém sódio"] } } },
 );
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
